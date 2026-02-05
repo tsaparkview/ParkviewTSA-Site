6501cc70e59a192ee05ffe11b7888620a56daf3d
Gimmy <gimmys943@gmail.com>
2026-02-04 22:54:46 -0500

    Remove duplicate ParkviewTSA-Site-main directory

--- ParkviewTSA-Site-main/public/js/index.js
+++
@@ -1,8 +0,0 @@
-// Import dotenv to load environment variables
-import 'dotenv/config';
-
-// Get the access key from environment variables
-const accessKey = process.env.ACCESS_KEY;
-
-// Set the value of the access_key input field
-document.getElementById('access_key').value = accessKey;
